a18757c80d0ddb6607980849568883d355cc164f
Cong <125168053+buiftheescoong@users.noreply.github.com>
2024-12-07 00:03:01 +0700

    fix handle many requests with zip

--- CS2/index.js
+++ CS2/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require("multer");
+const crypto = require("crypto");
 
 const { unzipFile } = require("./utils/unzip");
 const { createZipFile } = require("./utils/zip");
@@ -16,12 +17,18 @@ app.use(express.static("public"));
 app.post("/upload", upload.single("file"), async (req, res) => {
     try {
         const inputPath = req.file.path;
-        const outputFolder = path.join(__dirname, "output");
-        const extractFolder = path.join(__dirname, "uploads", "extracted");
+        const requestId =  crypto.randomUUID();
+      
 
         const fileType = path.extname(req.file.originalname).toLowerCase();
 
         if (fileType === ".zip") {
+            const extractFolder = path.join(__dirname, "uploads", `extracted_${requestId}`);
+            const outputFolder = path.join(__dirname, "output", `output_${requestId}`);
+    
+    
+            fs.mkdirSync(extractFolder, { recursive: true });
+            fs.mkdirSync(outputFolder, { recursive: true });
             await unzipFile(inputPath, extractFolder);
             const extractedFiles = fs.readdirSync(extractFolder);
             const pdfFiles = [];
@@ -35,24 +42,23 @@ app.post("/upload", upload.single("file"), async (req, res) => {
                     pdfFiles.push(pdfPath);
                 }
             }
-
-            const zipOutputPath = path.join(outputFolder, "processed_files.zip");
+            
+            const zipOutputPath = path.join(outputFolder, "processed_file.zip");          
             await createZipFile(zipOutputPath, pdfFiles);
-
             fs.unlinkSync(inputPath);
             fs.rmSync(extractFolder, { recursive: true, force: true });
 
-            res.json({ downloadLink: `/download/${path.basename(zipOutputPath)}` });
+            res.json({ downloadLink: `/download/${requestId}` });
 
         } else if (fileType === ".jpg" || fileType === ".jpeg" || fileType === ".png") {
-        
+            
             const text = await ocr.image2text(inputPath);
             const translatedText = await translate(text);
             const pdfPath = createPDF(translatedText, req.file.originalname);
 
             fs.unlinkSync(inputPath);
 
-            res.json({ downloadLink: `/download/${path.basename(pdfPath)}` });
+            res.json({ downloadLink: `/downloadFile/${path.basename(pdfPath)}` });
 
         } else {
             fs.unlinkSync(inputPath);
@@ -63,10 +69,18 @@ app.post("/upload", upload.single("file"), async (req, res) => {
         res.status(500).send("Internal Server Error");
     }
 });
-
-app.get("/download/:filename", (req, res) => {
+app.get("/downloadFile/:filename", (req, res) => {
     const zipPath = path.join(__dirname, "output", req.params.filename);
-    
+    res.download(zipPath, (err) => {
+            if (err) {
+                console.error("Error sending PDF:", err);
+            }
+            fs.unlinkSync(zipPath);
+        });
+});
+app.get("/download/:requestId", (req, res) => {
+    const requestId = req.params.requestId;
+    const zipPath = path.join(__dirname, "output", `output_${requestId}`, "processed_file.zip");
     res.download(zipPath, (err) => {
             if (err) {
                 console.error("Error sending PDF:", err);
@@ -77,4 +91,4 @@ app.get("/download/:filename", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-});
+});     
